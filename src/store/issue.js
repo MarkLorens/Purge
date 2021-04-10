87aa409241f5125635a8a5a66adcc9d57023abd9
Mark Lorens <57747268+MarkLorens@users.noreply.github.com>
2021-04-10 14:26:13 +0800

    Implement Index

---
+++ src/store/issue.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export default {
+    namespaced: true,
+    state: {
+        issues: []
+    },
+    getters : {
+        allIssues: (state) => state.issues
+    },
+    mutations: {
+        SET_ISSUES: (state, issues) =>
+            state.issues = issues
+    },
+    actions: {
+        async fetchIssues({ commit}) {
+            const response = await axios.get('/v1/protected/issue/index', {
+                headers: {
+                    token: localStorage.getItem('token'),
+                    userId: localStorage.getItem('uID')
+                }
+            })
+            commit('SET_ISSUES', response.data.data)
+        }
+    }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <Navbar />
   <Sidebar />
-  <div class="home">
-    <h3>Home</h3>
+  <div class="issues">
+    <div class="issue" v-for="issue in allIssues" :key="issue.id">
+      <span> {{ issue.Title }} </span>
+      <span>Severity: {{ issue.Severity }}; Status: {{ issue.Status }}</span>
+      <span>Updated: {{ issue.UpdatedAt }}</span>
+      <span class="created">{{ issue.CreatedAt }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
 import Sidebar from '../components/Sidebar.vue'
-// import axios from 'axios'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Home',
@@ -17,22 +22,49 @@ export default {
     Navbar,
     Sidebar
   },
-  // created() {
-  //   const res = axios.get('/v1/protected/issue/show/1',{
-  //     headers: {
-  //       token: localStorage.getItem('token'),
-  //       userID: localStorage.getItem('uID')
-  //     }
-  //   })
-  // }
+  methods: {
+    ...mapActions({
+      fetchIssues: 'issue/fetchIssues'
+    })
+  },
+  computed: {
+    ...mapGetters({
+      allIssues: 'issue/allIssues'})
+  },
+  created() {
+    this.fetchIssues()
+    console.log(this.allIssues);
+  }
 }
 </script>
 
 <style scoped>
-.home {
-  left: 10;
+.issues {
+  display: grid;
+  max-width: 800px;
+  margin: 20px auto;
+  margin-left: 20rem;
+  grid-template-columns: repeat(1);
+  gap: 2rem;
 }
-h3 {
-  color: #66bde5;
+.issue {
+  border: 1px solid black;
+  background: #eee;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: left;
+  position: relative;
+  cursor: pointer;
+}
+.created {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: black;
+  cursor: pointer;
+}
+span {
+  margin-top: 3px;
+  display: block;
 }
 </style>
