63339be8d9aa8ff7d70040c50806f35d7fc3fd6a
Mark Lorens <57747268+MarkLorens@users.noreply.github.com>
2021-04-11 16:41:52 +0800

    H12

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+// Server file
 const express = require('express')
 const serveStatic = require('serve-static')
-const history = require('connect-history-api-fallback')
-const enfore = require('express-sslify')
 const path = require('path')
-
-const app = express();
-
-app.use(enfore.HTTPS({ trustProtoHeader: true }));
-app.use(serveStatic(__dirname + '/dist'));
-app.use(history());
-
-app.listen(process.env.PORT || 5000);
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express()
+// All urls goto to index.html in /dist folder [build folder]
+app.use("/", serveStatic(path.join(__dirname, '/dist')))
+app.get(/.*/, function (req, res) {
+res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+const port = process.env.PORT || 8081;
+app.listen(port)
+console.log(`App is listening on port: ${port}`)
