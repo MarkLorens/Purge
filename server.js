cfff6a13f5554986b6d42fee5e1b9d938827adef
Mark Lorens <57747268+MarkLorens@users.noreply.github.com>
2021-04-11 15:06:07 +0800

    Deployment config

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
+const enfore = require('express-sslify')
+
+const app = express();
+
+app.use(enfore.HTTPS({ trustProtoHeader: true }));
+app.use(serveStatic(__dirname + '/dist'));
+app.use(history);
+
+app.listen(process.env.PORT || 5000);
